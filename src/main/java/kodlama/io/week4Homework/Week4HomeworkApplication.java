901bb2a70a33184e31d5fd1f52f793d937feb0ea
kubilaybee <kubilaybee@gmail.com>
2022-10-28 02:03:24 +0300

    start the project

---
+++ src/main/java/kodlama/io/week4Homework/Week4HomeworkApplication.java
@@ -0,0 +1,13 @@
+package kodlama.io.week4Homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week4HomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week4HomeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kodlama/io/week4Homework/Week4HomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package kodlama.io.week4Homework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week4HomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
