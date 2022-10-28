fc71e0ea265b0a38b38d09ccd53db45ac7500f5d
kubilaybee <kubilaybee@gmail.com>
2022-10-28 15:10:01 +0300

    entities SoftwareLanguage added

--- src/main/java/kodlama/io/week4Homework/entities/concretes/SoftwareLanguage.java
+++ src/main/java/kodlama/io/week4Homework/entities/concretes/SoftwareLanguage.java
@@ -1,4 +1,30 @@
 package kodlama.io.week4Homework.entities.concretes;
 
 public class SoftwareLanguage {
+    private int id;
+    private String name;
+
+    public SoftwareLanguage() {
+    }
+
+    public SoftwareLanguage(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
