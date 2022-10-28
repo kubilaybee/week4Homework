5386291b09ac7ff7f7d0e094fc57ac20b4bd154d
kubilaybee <kubilaybee@gmail.com>
2022-10-28 15:31:19 +0300

    dataAccess SoftwareLanguageRepository interface added

--- src/main/java/kodlama/io/week4Homework/dataAccess/abstracts/SoftwareLanguageRepository.java
+++ src/main/java/kodlama/io/week4Homework/dataAccess/abstracts/SoftwareLanguageRepository.java
@@ -1,4 +1,18 @@
 package kodlama.io.week4Homework.dataAccess.abstracts;
 
+import kodlama.io.week4Homework.entities.concretes.SoftwareLanguage;
+
+import java.util.List;
+
 public interface SoftwareLanguageRepository {
+
+    void add(SoftwareLanguage language);
+
+    void delete(int id);
+
+    void update(SoftwareLanguage language);
+
+    SoftwareLanguage getById(int id);
+
+    List<SoftwareLanguage> getAll();
 }
