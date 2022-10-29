a7f4c18769f42531990cabac206094322853bcbf
kubilaybee <kubilaybee@gmail.com>
2022-10-29 07:40:55 +0300

    web api step started

--- src/main/java/kodlama/io/week4Homework/webApi/controllers/SoftwareLanguageControllers.java
+++ src/main/java/kodlama/io/week4Homework/webApi/controllers/SoftwareLanguageControllers.java
@@ -1,4 +1,18 @@
 package kodlama.io.week4Homework.webApi.controllers;
 
+import kodlama.io.week4Homework.business.abstracts.SoftwareLanguageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/softwarelanguages")
 public class SoftwareLanguageControllers {
+    private SoftwareLanguageService languageService;
+
+    @Autowired
+    public SoftwareLanguageControllers(SoftwareLanguageService languageService) {
+        this.languageService = languageService;
+    }
+    // --
 }
